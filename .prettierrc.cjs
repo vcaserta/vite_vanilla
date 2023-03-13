15bb653c4f47a95010d3456d99a0806e59c172fa
vcaserta <vincenzo.caserta@soprasteria.com>
2023-03-13 17:04:26 +0100

    marquee edit2

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,8 +1,9 @@
 module.exports = {
-  trailingComma: 'es5',
+  trailingComma: 'all',
   tabWidth: 2,
   semi: true,
   singleQuote: true,
   endOfLine: 'auto',
   plugins: [require('prettier-plugin-tailwindcss')],
+
 };
