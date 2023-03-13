cccef4a89481e3e176e157db859260c73783d160
vcaserta <vincenzo.caserta@soprasteria.com>
2023-03-13 14:56:37 +0100

    render edit 2

---
+++ src/js/edit2.js
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+import Swiper, { EffectFade } from 'swiper';
+
+$(() => {
+  const swiper = new Swiper('#edit2Swiper', {
+    modules: [EffectFade],
+    slidesPerView: 1,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true,
+    },
+  });
+
+  $('.js-edt-2-pin').on('click', function () {
+    $(this).addClass('active');
+    const index = $(this).data('product');
+    swiper.slideTo(index);
+    $(this).siblings().removeClass('active');
+  });
+});
--- src/main.js
+++ src/main.js
@@ -2,3 +2,4 @@ import './index.scss';
 
 import './js/home';
 import './js/edit4';
+import './js/edit2';
