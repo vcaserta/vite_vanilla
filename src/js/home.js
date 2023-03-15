23534d93d987f75b9e780df3f83c6251090e1504
vcaserta <vincenzo.caserta@soprasteria.com>
2023-03-15 09:17:22 +0100

    marquee carousel

--- src/js/home.js
+++ src/js/home.js
@@ -6,7 +6,7 @@ import { gsap } from 'gsap';
 import { ScrollMagicPluginGsap } from 'scrollmagic-plugin-gsap';
 import 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators';
 
-import Swiper, { Autoplay, EffectCreative, EffectFade, FreeMode } from 'swiper';
+import Swiper, { Autoplay } from 'swiper';
 import 'slick-carousel';
 // eslint-disable-next-line no-multi-assign
 window.$ = window.jQuery = $;
@@ -150,102 +150,4 @@ $(() => {
   //   .addIndicators()
   //   .addTo(controller);
   // End Carousel Animation
-
-  const carouselOptions = {
-    modules: [EffectCreative],
-    slidesPerView: 2.2,
-    effect: 'creative',
-    centeredSlides: true,
-    nested: true,
-    loop: true,
-    creativeEffect: {
-      limitProgress: 10,
-      progressMultiplier: 2,
-      prev: {
-        translate: ['-50%', '1.5%', 120],
-      },
-      next: {
-        translate: ['68%', '-1%', -250],
-      },
-    },
-  };
-
-  const swiper4 = new Swiper('#edit1SwiperInn1', carouselOptions);
-  const swiper5 = new Swiper('#edit1SwiperInn2', carouselOptions);
-  const swiper6 = new Swiper('#edit1SwiperInn3', carouselOptions);
-
-  const swiperMain = new Swiper('#edit1Main', {
-    modules: [EffectFade],
-    slidesPerView: 1,
-    allowTouchMove: false,
-    effect: 'fade',
-    fadeEffect: {
-      crossFade: true,
-    },
-    loop: true,
-  });
-  const swiper1 = new Swiper('#edit1Swiper', {
-    modules: [Autoplay, FreeMode],
-    slidesPerView: 'auto',
-    loop: true,
-    autoplay: {
-      delay: 0,
-      pauseOnMouseEnter: true,
-      disableOnInteraction: true,
-    },
-    grabCursor: true,
-    speed: 5000,
-    freeMode: true,
-    centeredSlides: true,
-  });
-  $('#edit1Swiper .swiper-slide').on('click', function (e) {
-    const index = this.getAttribute('data-index');
-    const slideActive = document.querySelectorAll('.slide-active');
-    slideActive.forEach((elm) => {
-      elm.classList.remove('slide-active');
-    });
-    const nextSlideActive = document.querySelectorAll(
-      `[data-index="${index}"]`
-    );
-    nextSlideActive.forEach((elm) => {
-      elm.classList.add('slide-active');
-    });
-    swiperMain.slideTo(index);
-  });
-  // swiper1.on('slideChange', () => {
-  //   swiperMain.slideNext();
-  // });
-  // const $slickCarousel = $('#edit1Slick');
-  // $slickCarousel.slick({
-  //   speed: 9000,
-  //   autoplay: true,
-  //   autoplaySpeed: 0,
-  //   cssEase: 'linear',
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   variableWidth: true,
-  //   infinite: true,
-  //   centerMode: true,
-  //   swipeToSlide: true,
-  //   touchThreshold: 100,
-  // });
-  // let prevStyle;
-  // $slickCarousel.on('mouseenter', function () {
-  //   $(this).slick('slickPause');
-  //   const $slickTrack = $(this).find('.slick-track');
-  //   const computedStyle = window.getComputedStyle($slickTrack[0]);
-  //   prevStyle = computedStyle.getPropertyValue('transform');
-  //   $slickTrack.css('transform', prevStyle);
-  //   // prevStyle = $slickTrack.attr('style');
-  //   $slickTrack.css('transition', 'none');
-  // });
-  // $slickCarousel.on('mouseleave', function () {
-  //   $(this).slick('slickPlay');
-  //   const $slickTrack = $(this).find('.slick-track');
-  //   $slickTrack.css('transform', prevStyle);
-  //   $slickTrack.css('transition', 'transform 9000ms linear 0s');
-  // });
-  // $slickCarousel.on('afterChange', function (slick, currentSlide) {
-  //   swiperMain.slideNext();
-  // });
 });
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './index.scss';
 
 import './js/home';
+import './js/edit1';
 import './js/edit4';
 import './js/edit2';
