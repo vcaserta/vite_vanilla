2247b5cdf5d97b67c58083e852ccf335788971f6
sbarbato <silvio.barbato@soprasteria.com>
2023-03-15 15:46:37 +0100

    HP editoriale 4

--- src/js/edit2.js
+++ src/js/edit2.js
@@ -1,21 +1,16 @@
 import $ from 'jquery';
-import Swiper, { EffectFade } from 'swiper';
 
 $(() => {
-  const swiper = new Swiper('#edit2Swiper', {
-    modules: [EffectFade],
-    slidesPerView: 1,
-    allowTouchMove: false,
-    effect: 'fade',
-    fadeEffect: {
-      crossFade: true,
-    },
-  });
-
   $('.js-edt-2-pin').on('click', function () {
     $(this).addClass('active');
     const index = $(this).data('product');
-    swiper.slideTo(index);
+    // swiper.slideTo(index);
+    document.querySelectorAll('.edit-2-prd-pin').forEach((elm) => {
+      elm.classList.add('!hidden');
+    });
+    document
+      .querySelector(`.edit-2-prd-pin[data-product="${index}"]`)
+      .classList.remove('!hidden');
     $(this).siblings().removeClass('active');
   });
 });
